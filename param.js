448c4e1cea86e42dab8488950912a40aa2168b03
varuntyagiG <varungauravtyagi@gmail.com>
2024-06-09 16:59:08 +0530

    revision

---
+++ param.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.listen(3001);
+
+// params
+app.get("/file/:filename", (req, res) => {
+  let { filename } = req.params;
+  fs.readFile(filename, "utf-8", (err, data) => {
+    console.log(err);
+    res.json({
+      data,
+    });
+  });
+});
