94bf8db5d8ed30662d7fdd11874dd45d35fd4478
varuntyagiG <varungauravtyagi@gmail.com>
2024-06-10 18:46:21 +0530

    node.js

--- MiddleWares.js
+++ MiddleWares.js
@@ -36,12 +36,36 @@ function numberofRequests(req, res, next) {
   next();
 }
 
-app.get(
-  "/healthy-check",
-  numberofRequests,
+app.use(express.json());
+app.use(numberofRequests); // app.use => work in each-route which is define after this-
+
+let wrapAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+app.get("/healthy-check", UserCheckUp, kidneyCheckup, (req, res) => {
+  res.send("Check-up");
+});
+
+// kidneyCheckup do not work in this route
+app.post(
+  "/heart-check",
   UserCheckUp,
-  kidneyCheckup,
-  (req, res) => {
-    res.send("Check-up");
-  },
+  wrapAsync((req, res) => {
+    let { kidneys } = req.body;
+    let kidneyslength = kidneys.length;
+    res.send("no of kidneys in your body" + kidneyslength);
+  }),
 );
+
+// error handlor
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(404).json({
+    mssge: "something went wrong",
+  });
+});
